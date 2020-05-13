00963b972388b612cffe03ec23439e88d60dc433
Marcelo Boeira <me@marceloboeira.com>
2020-05-13 15:47:28 +0200

    Setup Terraform with AWS account
    - Configure terraform
    - Configure provider plugin
    - Setup credentials on shared
    - Add credentials sample

---
+++ solutions-architect-associate/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-central-1"
+  shared_credentials_file = "../shared/credentials"
+}
+
+resource "aws_iam_user" "first-user" {
+  name = "first-user"
+}
