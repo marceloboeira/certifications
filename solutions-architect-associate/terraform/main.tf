0ce11d50e9c22f1037b675bda6c80c94fe015c8a
Marcelo Boeira <me@marceloboeira.com>
2020-05-14 22:51:13 +0200

    Add different users / TODOs

--- solutions-architect-associate/terraform/main.tf
+++ solutions-architect-associate/terraform/main.tf
@@ -4,6 +4,19 @@ provider "aws" {
   shared_credentials_file = "../../shared/credentials"
 }
 
-resource "aws_iam_user" "first-user" {
-  name = "first-user"
+# TODO: Import Organization
+# https://www.terraform.io/docs/providers/aws/r/organizations_organization.html
+# https://www.terraform.io/docs/providers/aws/r/organizations_account.html
+
+# Limited User
+resource "aws_iam_user" "max-mustermann" {
+  name = "max-mustermann"
+}
+
+# Admin User (equivalent to a root account permission)
+resource "aws_iam_user" "erika-mustermann" {
+  name = "erika-mustermann"
+
+  # TODO
+  #  Add global admin permission
 }
