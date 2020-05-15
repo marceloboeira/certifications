2d962c8a8cf2360011b4d82ef621acf0dff32286
Marcelo Boeira <me@marceloboeira.com>
2020-05-15 16:55:19 +0200

    Use variables for account names

--- solutions-architect-associate/terraform/03_organizations.tf
+++ solutions-architect-associate/terraform/03_organizations.tf
@@ -3,16 +3,16 @@ resource "aws_organizations_organization" "mustermann_gmbh" {
 }
 
 resource "aws_organizations_account" "root_account" {
-  name  = "marceloboeira-certification"
+  name  = var.root_account_name
   email = var.root_account_email
 }
 
 resource "aws_organizations_account" "staging" {
-  name  = "staging"
+  name  = var.staging_account_name
   email = var.staging_account_email
 }
 
 resource "aws_organizations_account" "production" {
-  name  = "production"
+  name  = var.production_account_name
   email = var.production_account_email
 }
--- solutions-architect-associate/terraform/vars.tf
+++ solutions-architect-associate/terraform/vars.tf
@@ -1,3 +1,8 @@
+variable "root_account_name" {}
 variable "root_account_email" {}
-variable "production_account_email" {}
+
+variable "staging_account_name" {}
 variable "staging_account_email" {}
+
+variable "production_account_name" {}
+variable "production_account_email" {}
