62a2398e22a726df5ed66f407e91fe40ebd1114a
Marcelo Boeira <me@marceloboeira.com>
2020-05-19 02:07:36 +0200

    Fix plugins version

--- solutions-architect-associate/terraform/main.tf
+++ solutions-architect-associate/terraform/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  version                 = "~> 2.8"
+  version                 = "~> 2"
   region                  = "eu-central-1"
   shared_credentials_file = "../../shared/credentials"
 }
 
 provider "aws" {
   alias                   = "replica"
-  version                 = "~> 2.8"
+  version                 = "~> 2"
   region                  = "eu-west-1"
   shared_credentials_file = "../../shared/credentials"
 }
